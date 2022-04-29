461e03deefb0906bcc9cdbddc1ccbc11c7f100a3
Abin KS <abinks056@gmail.com>
2022-04-29 20:53:53 +0530

    frontend completed

--- salesrecords.py
+++ salesrecords.py
@@ -1,32 +1,55 @@
-from json import load
-from tkinter import *
-from tkinter import ttk
 import tkinter as tk
+from tkinter import *
+from  tkinter import ttk
+import tkinter.font as font
+from tkinter import messagebox as MessageBox
+import mysql.connector as mysql
+from PIL import Image,ImageTk
+from tkinter.ttk import Combobox
+from tkcalendar import Calendar, DateEntry
+def add_custom():
+    import addcustomer_form
+
 root = tk.Tk()
+root.title("finsYs")
 width=root.winfo_screenwidth()
 height=root.winfo_screenheight()
 root.geometry("%dx%d" %(width,height))
 root.configure(bg="#2f516f")
+wrappen=ttk.LabelFrame(root)
+mycanvas=Canvas(wrappen)
+mycanvas.pack(side=LEFT,fill="both",expand="yes")
+yscrollbar=ttk.Scrollbar(wrappen,orient='vertical',command=mycanvas.yview)
+yscrollbar.pack(side=RIGHT,fill='y')
 
-root.title("Finsys")
-bg_color = "#badc57"
+mycanvas.configure(yscrollcommand=yscrollbar.set)
+mycanvas.bind('<Configure>',lambda e:mycanvas.configure(scrollregion=mycanvas.bbox('all')))
 
-N = Label(root, bg="#243e55", fg="#fff",font=('times new roman', 10, 'bold'), relief=RAISED)
-N.place(relx=0.1, rely=0.1, relheight=0.1, relwidth=0.8)
-tit = Label(N, text="SALES RECORDS", font=('times new roman', 28, 'bold'), pady=2, bd=12, bg="#243e55", fg="#fff", relief=GROOVE)
-tit.pack(fill=X)
+full_frame=Frame(mycanvas,width=1345,height=2200,bg='#2f516a')
+mycanvas.create_window((0,0),window=full_frame,anchor="nw")
 
-F = LabelFrame(root, font=('times new roman', 15, 'bold'), bd=10, fg="Black", bg="#243e55")
-F.place(x=5, y=200, width=1345, height=400)
+heading_frame=Frame(mycanvas)
+mycanvas.create_window((0,50),window=heading_frame,anchor="nw")
+
+form_frame=Frame(mycanvas,width=1340,height=1900,bg='#243e55')
+mycanvas.create_window((3,150),window=form_frame,anchor="nw")
+form_lable=tk.Label(form_frame,bg="#243e55",width=100)
+form_lable.place(x=0,y=0)
+
+tit = Label(heading_frame, text="SALES RECORDS", font=('times new roman', 28, 'bold'),padx=504, pady=2, bd=12, bg="#243e55", fg="#fff", relief=GROOVE)
+tit.pack()
+
+F = LabelFrame(form_frame, font=('times new roman', 15, 'bold'), bd=10, fg="Black", bg="#243e55")
+F.place(x=20, y=50, width=1300, height=400)
 
 menu= StringVar()
 menu.set("New Transaction")
 
 #Create a dropdown Menu
-drop= OptionMenu(F, menu,"Invoice", "Payment","Sales Receipt","Credit note","Estimate","Delayed Charge","Time Activity")
-drop.grid(row=1, column=0, padx=10, pady=10)
+drop= OptionMenu(form_frame, menu,"Invoice", "Payment","Sales Receipt","Credit note","Estimate","Delayed Charge","Time Activity")
+drop.place(x=1150,y=10,width=150)
 
-tree = ttk.Treeview(F,height=10)
+tree = ttk.Treeview(F,height=15)
 tree['show'] = 'headings'
 
 sb = ttk.Scrollbar(F, orient="vertical", command=tree.yview)
@@ -36,16 +59,16 @@ tree.configure(yscrollcommand=sb.set)
 
 tree["columns"]=("1","2","3","4","5","6","7","8","9","10")
 
-tree.column("1", width=130)
-tree.column("2", width=130)
-tree.column("3", width=130)
-tree.column("4", width=130)
-tree.column("5", width=130)
-tree.column("6", width=130)
-tree.column("7", width=130)
-tree.column("8", width=130)
-tree.column("9", width=130)
-tree.column("10", width=130)
+tree.column("1", width=125)
+tree.column("2", width=125)
+tree.column("3", width=125)
+tree.column("4", width=125)
+tree.column("5", width=125)
+tree.column("6", width=125)
+tree.column("7", width=125)
+tree.column("8", width=125)
+tree.column("9", width=125)
+tree.column("10", width=125)
 
 tree.heading("1", text="DATE")
 tree.heading("2", text="TYPE")
@@ -60,11 +83,16 @@ tree.heading("10", text="ACTION")
 datas = ["22-10-2022","artist","1","Alen","12-10-2022","500000","235600","25","725000","ACTION"]
 item = tree.insert("", "end", values=(datas))
 
-tree.grid(row=2,column=0,padx=5,pady=5)
+tree.grid(row=1,column=0,padx=5,pady=5)
 
+wrappen.pack(fill='both',expand='yes',)
+
+wrappen.pack(fill='both',expand='yes',)
+
+wrappen.pack(fill='both',expand='yes',)
+
+ 
 
 root.mainloop()
-
-
 
 
