1f910a53c966eb91ff9d4d99946621649c6c583b
sheryps <sherrygeorge48@gmail.com>
2022-04-29 19:02:12 +0530

    second commit

--- finsyssuppliers.py
+++ finsyssuppliers.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import VERTICAL, ttk
+from tkinter import messagebox
 import tkinter.font as font
 from tkcalendar import DateEntry,Calendar
+import re
 #database connection
 import mysql.connector
 mydata=mysql.connector.connect(host='localhost', user='root', password='', database='finsys_tkinter')
@@ -100,11 +102,11 @@ def addsuppliers():
         fname=efname.get()
         lname=elname.get()
         comp=ecomp.get()
-        mail=e_mail.get()
-        mob=emobile.get()
+        mail=eemail.get()
+        mob=mb.get()
         open=eopen.get()
         acc=e_accno.get()
-        webs=webb.get()
+        webs=web.get()
         bill=ebill.get()
         terms=eterms.get()
         gst=egst.get()
@@ -120,13 +122,41 @@ def addsuppliers():
         contry=econt.get()
         note=enotes.get()
         bn=bm.get()
-        tg='''INSERT INTO supplier (title,firstname,lastname,company,mobile,email,website,billingrate,terms,addterms,openingbalance,accountno,gsttype,
-        gstin,taxregisterationno,effectivedate,defaultexpenceaccount,tds,street,city,state ,pincode,country,notes) 
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
-        cur.execute(tg,[(title),(fname),(lname),(comp),(mob),(mail),(webs),(bill),(terms),(bn),(open),(acc),(gst),(gst_in),(tax),(date),(defexp),
-        (tds),(street),(city),(state),(pin),(contry),(note)])
-        mydata.commit()
-        B.destroy()
+        ckh=chh.get()
+        print(mob,fname)
+        if fname=='':
+            messagebox.showerror('Error','Please enter your first',parent=B)
+        elif lname=='':
+            messagebox.showerror('Error','Please enter your lastname',parent=B)
+        elif comp=='':
+            messagebox.showerror('Error','Please enter your company',parent=B)   
+        elif mail=='':
+            messagebox.showerror('Error','Please enter your mail',parent=B)  
+        elif mob=='':
+            messagebox.showerror('Error','Please enter your mobile number',parent=B)    
+        elif open=='':
+            messagebox.showerror('Error','Please enter your opening balance',parent=B)
+        elif acc=='':
+            messagebox.showerror('Error','Please enter your account number',parent=B)  
+    
+            messagebox.showerror('Error','Please enter a valid city',parent=B)       
+        elif street=='':
+                  chk_bl.config(text='Please enter a valid street',fg='red')  
+        elif city=='':
+                  chk_b2.config(text='Please enter a valid City',fg='red') 
+        elif pin=='':
+                  chk_b3.config(text='Please enter  pincode',fg='red')
+        elif ckh==0:
+                  chk_b4.config(text='Please agree to terms and conditions',fg='red') 
+        else:
+            chk_b4.config(text='checked',fg='green')                                                  
+        #tg='''INSERT INTO supplier (title,firstname,lastname,company,mobile,email,website,billingrate,terms,addterms,openingbalance,accountno,gsttype,
+        #gstin,taxregisterationno,effectivedate,defaultexpenceaccount,tds,street,city,state ,pincode,country,notes) 
+        #VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+        #cur.execute(tg,[(title),(fname),(lname),(comp),(mob),(mail),(webs),(bill),(terms),(bn),(open),(acc),(gst),(gst_in),(tax),(date),(defexp),
+        #(tds),(street),(city),(state),(pin),(contry),(note)])
+        #mydata.commit()
+        #B.destroy()
 
     global B
     B=tk.Toplevel(A)
@@ -140,9 +170,9 @@ def addsuppliers():
     mycanvas.bind('<Configure>',lambda e:mycanvas.configure(scrollregion=mycanvas.bbox('all')))
     frame=tk.Frame(mycanvas)
     frame['bg']='#2f516f'
-    mycanvas.create_window((0,0),window=frame,anchor='nw',width=1500,height=1000)
+    mycanvas.create_window((0,0),window=frame,anchor='nw',width=1500,height=1200)
         #head frame
-
+    
     head1 = tk.LabelFrame(frame,borderwidth=0,bg='#243e54')
     f1 = font.Font(family='Times New Roman',size=30)#font
     lb1=tk.Label(head1,text='ADD SUPPLIERS',bg='#243e54')
@@ -159,84 +189,130 @@ def addsuppliers():
     label1['font']=f2
     label1.place(relx=0.01,rely=0)
 
-    tk.Label(hd1,text='Title',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.05)
+    tk.Label(hd1,text='Title',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.045)
     #title.grid(row=3,column=2,padx=20,pady=20)
     cont=['Mr','Mrs','Miss','Ms']
     cmb=ttk.Combobox(hd1,values=cont)
     cmb.current(0)
-    cmb.place(relx=0.02,rely=0.08,relwidth=0.3,relheight=0.035)
+    cmb.place(relx=0.02,rely=0.075,relwidth=0.3,relheight=0.025)
 
-    tk.Label(hd1,text='First Name',bg='#243e54',font=('times new roman', 14)).place(relx=0.35,rely=0.05)
-    efname=StringVar()
-    tk.Entry(hd1,textvariable=efname).place(relx=0.35,rely=0.08,relwidth=0.3,relheight=0.035)
+    tk.Label(hd1,text='First Name',bg='#243e54',font=('times new roman', 14)).place(relx=0.35,rely=0.045)
+    efname=tk.Entry(hd1)
+    efname.place(relx=0.35,rely=0.075,relwidth=0.3,relheight=0.025)
 
-    tk.Label(hd1,text='Last Name',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.05)
-    elname=StringVar()
-    tk.Entry(hd1,textvariable=elname).place(relx=0.68,rely=0.08,relwidth=0.3,relheight=0.035)
+    tk.Label(hd1,text='Last Name',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.045)
+    elname=tk.Entry(hd1)
+    elname.place(relx=0.68,rely=0.075,relwidth=0.3,relheight=0.025)
+
     
     tk.Label(hd1,text='Company',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.13)
-    ecomp=StringVar()
-    tk.Entry(hd1,textvariable=ecomp).place(relx=0.02,rely=0.16,relwidth=0.3,relheight=0.035)
+    ecomp=tk.Entry(hd1)
+    ecomp.place(relx=0.02,rely=0.165,relwidth=0.3,relheight=0.025)
+
+    def checkmail(event):
+        mail=eemail.get()
+        print(mail)
+        if len(mail)>=10:
+            print('checked')
+            if re.match("^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$",mail):
+                em_bl.config(text='Looks Good',fg='green') 
+                return True
+            else:
+                em_bl.config(text='Enter Valid Email',fg='RED')
+                return False
+        else:
+
+            em_bl.config(text='Email too Short ',fg='red')
+            return False           
+
 
     tk.Label(hd1,text='Email',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.13)
-    e_mail=StringVar()
-    eemail=tk.Entry(hd1,textvariable=e_mail)
+    eemail=tk.Entry(hd1)
     eemail.insert(0,'example@gmail.com')
-    eemail.place(relx=0.35,rely=0.16,relwidth=0.3,relheight=0.035)
+    eemail.bind('<FocusOut>',checkmail)
+    eemail.place(relx=0.35,rely=0.165,relwidth=0.3,relheight=0.025)
+
+    em_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    em_bl.place(relx=0.35,rely=0.19)
+
+    def valid_int(inp):
+        if inp.isdigit():
+                mb_bl.config(text='Looks Good',fg='green') 
+                return True          
+        else:
+            mb_bl.config(text='Please Provide Valid Number',fg='red') 
+            return False
 
     tk.Label(hd1,text='Mobile',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.13)
-    emobile=StringVar()
-    tk.Entry(hd1,textvariable=emobile).place(relx=0.68,rely=0.16,relwidth=0.3,relheight=0.035)
+    mb=tk.Entry(hd1)
+    mb.place(relx=0.68,rely=0.165,relwidth=0.3,relheight=0.025)
+    val=B.register(valid_int)
+    mb.config(validate='key',validatecommand=(val,"%P"))
+
+    mb_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    mb_bl.place(relx=0.68,rely=0.19)
+
     
-    tk.Label(hd1,text='Opening Balance',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.20)
-    eopen=StringVar()
-    tk.Entry(hd1,textvariable=eopen).place(relx=0.02,rely=0.24,relwidth=0.3,relheight=0.035)
+   
+    tk.Label(hd1,text='Opening Balance',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.21)
+    eopen=tk.Entry(hd1)
+    eopen.place(relx=0.02,rely=0.235,relwidth=0.3,relheight=0.025)
+    eopen.config(validate='key',validatecommand=(val,"%P"))
 
-    tk.Label(hd1,text='Account No:',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.20)
-    e_accno=StringVar()
-    tk.Entry(hd1,textvariable=e_accno).place(relx=0.35,rely=0.24,relwidth=0.3,relheight=0.035)
+    mb_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    mb_bl.place(relx=0.02,rely=0.26)
 
-    web=tk.Label(hd1,text='Website',font=('times new roman', 14),bg='#243e54')
-    web.place(relx=0.68,rely=0.20)
-    webb=StringVar()
-    eweb=tk.Entry(hd1,textvariable=webb)
-    eweb.insert(0,'www.example.com')
-    eweb.place(relx=0.68,rely=0.24,relwidth=0.3,relheight=0.035)
+    tk.Label(hd1,text='Account No:',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.21)
+    e_accno=tk.Entry(hd1)
+    e_accno.place(relx=0.35,rely=0.235,relwidth=0.3,relheight=0.025)
+    e_accno.config(validate='key',validatecommand=(val,"%P"))
+
+    #mb_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    #mb_bl.place(relx=0.35,rely=0.26)
+
+    tk.Label(hd1,text='Website',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.21)
+    web=tk.Entry(hd1)
+    web.insert(0,'www.example.com')
+    web.place(relx=0.68,rely=0.235,relwidth=0.3,relheight=0.025)
+    #web.config(validate='key',validatecommand=(val,"%P"))
+
+    #mb_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    #mb_bl.place(relx=0.68,rely=0.26)
 
     tk.Label(hd1,text='Billing Rate',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.28)
-    ebill=StringVar()
-    tk.Entry(hd1,textvariable=ebill).place(relx=0.02,rely=0.31,relwidth=0.3,relheight=0.035)
+    ebill=tk.Entry(hd1)
+    ebill.place(relx=0.02,rely=0.31,relwidth=0.3,relheight=0.025)
 
     tk.Label(hd1,text='Term',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.28)
     termvalues=['DUE ON RECEIPT','NET15','NET30','NET60','ADD NEW TERMS']
     eterms=ttk.Combobox(hd1,values=termvalues)
     eterms.current(0)
-    eterms.place(relx=0.35,rely=0.31,relwidth=0.3,relheight=0.035)
+    eterms.place(relx=0.35,rely=0.31,relwidth=0.3,relheight=0.025)
 
-    bm=StringVar()
-    tk.Entry(hd1,textvariable=bm).place(relx=0.68,rely=0.31,relwidth=0.3,relheight=0.035)
+    bm=tk.Entry(hd1)
+    bm.place(relx=0.68,rely=0.31,relwidth=0.3,relheight=0.025)
 
     tk.Label(hd1,text='GST Type',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.35)
-    gstvalues=['CHOOSE','GST registered-Regular','GST registered-Composition','GST-unregistered']
+    gstvalues=['GST registered-Regular','GST registered-Composition','GST-unregistered']
     egst=ttk.Combobox(hd1,values=gstvalues)
     egst.current(0)
-    egst.place(relx=0.02,rely=0.38,relwidth=0.3,relheight=0.035)
+    egst.place(relx=0.02,rely=0.38,relwidth=0.3,relheight=0.025)
 
     tk.Label(hd1,text='GST IN',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.35)
     gstin=StringVar()
     egst_in=tk.Entry(hd1,textvariable=gstin)
     egst_in.insert(0,'22AAAAA0000A1Z5')
-    egst_in.place(relx=0.35,rely=0.38,relwidth=0.3,relheight=0.035)
+    egst_in.place(relx=0.35,rely=0.38,relwidth=0.3,relheight=0.025)
 
     taxreg=tk.Label(hd1,text='Tax Registeration N0',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.35)
-    etaxreg=StringVar()
-    tk.Entry(hd1,textvariable=etaxreg).place(relx=0.68,rely=0.38,relwidth=0.3,relheight=0.035)  
+    etaxreg=tk.Entry(hd1)
+    etaxreg.place(relx=0.68,rely=0.38,relwidth=0.3,relheight=0.025)  
 
-    tk.Label(hd1,text='Effective Date',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.42)
+    tk.Label(hd1,text='Effective Date',font=('times new roman', 14),bg='#243e54').place(relx=0.02,rely=0.41)
     ddate=StringVar()
-    DateEntry(hd1,textvariable=ddate).place(relx=0.02,rely=0.45,relwidth=0.3,relheight=0.035)
+    DateEntry(hd1,textvariable=ddate).place(relx=0.02,rely=0.44,relwidth=0.3,relheight=0.025)
     
-    tk.Label(hd1,text='Default Expense Account',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.42)
+    tk.Label(hd1,text='Default Expense Account',font=('times new roman', 14),bg='#243e54').place(relx=0.35,rely=0.41)
     defvalues=['choose','Advertising/Promotional','Bank Charges','Business Licenses and Permitts','Charitable Contributions','Computer and Internet Expense','Continuing Education','Depreciation Expense','Dues and Subscriptions',
     'Housekeeping Charges','Insurance Expenses','Insurance Expenses-General Liability Insurance','Insurance Expenses-Health Insurance','Insurance Expenses-Life and Disability Insurance','Insurance Expenses-Professional Liability',
     'Internet Expenses','Meals and Entertainment','Office Supplies','Postage and Delivery','Printing and Reproduction','Professional Fees','Purchases','Rent Expense','Repair and Maintenance','Small Tools and Equipments',
@@ -244,27 +320,33 @@ def addsuppliers():
     'Reconciliation Discrepancies','SGST Write-off','Tax Write-off','Vehicle Expenses','Cost of Sales','Equipment Rental for Jobs','Freight and Shipping Cost','Merchant Account Fees','Purchases-Hardware For Resale','Purchases-Software For Resale','Subcontracted Services','Tools and Craft Suppliers']
     edefexp=ttk.Combobox(hd1,values=defvalues)
     edefexp.current(0)
-    edefexp.place(relx=0.35,rely=0.45,relwidth=0.27,relheight=0.035)  
+    edefexp.place(relx=0.35,rely=0.44,relwidth=0.27,relheight=0.025)  
 
-    tk.Button(hd1,text='+',font=(14),command=sherryplus).place(relx=0.625,rely=0.45,relwidth=0.025,relheight=0.035)
+    tk.Button(hd1,text='+',font=(14),command=sherryplus).place(relx=0.625,rely=0.44,relwidth=0.025,relheight=0.025)
 
-    tk.Label(hd1,text='Apply TDS for Supplier',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.42)
-    etds=StringVar()
-    tk.Entry(hd1,textvariable=etds).place(relx=0.68,rely=0.45,relwidth=0.3,relheight=0.035)  
+    tk.Label(hd1,text='Apply TDS for Supplier',font=('times new roman', 14),bg='#243e54').place(relx=0.68,rely=0.41)
+    etds=tk.Entry(hd1)
+    etds.place(relx=0.68,rely=0.44,relwidth=0.3,relheight=0.025)  
     
     label2=tk.Label(hd1,text='Address',bg='#243e54')
     label2['font']=f2
-    label2.place(relx=0.01,rely=0.49)
+    label2.place(relx=0.01,rely=0.47)
 
-    tk.Label(hd1,text='Street',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.53)
-    estreet=StringVar()
-    tk.Entry(hd1,textvariable=estreet).place(relx=0.02,rely=0.57,relwidth=0.63,relheight=0.035)  
+    tk.Label(hd1,text='Street',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.52)
+    estreet=tk.Entry(hd1)
+    estreet.place(relx=0.02,rely=0.55,relwidth=0.63,relheight=0.025)  
+
+    chk_bl=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    chk_bl.place(relx=0.02,rely=0.575)
     
-    tk.Label(hd1,text='City',bg='#243e54',font=('times new roman', 14)).place(relx=0.68,rely=0.53)
-    ecity=StringVar()
-    tk.Entry(hd1,textvariable=ecity).place(relx=0.68,rely=0.57,relwidth=0.3,relheight=0.035)
+    tk.Label(hd1,text='City',bg='#243e54',font=('times new roman', 14)).place(relx=0.68,rely=0.52)
+    ecity=tk.Entry(hd1)
+    ecity.place(relx=0.68,rely=0.55,relwidth=0.3,relheight=0.025)
 
-    tk.Label(hd1,text='State',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.61)
+    chk_b2=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    chk_b2.place(relx=0.68,rely=0.575)
+
+    tk.Label(hd1,text='State',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.60)
     stvalues=['choose','Andaman and Nicobar Islands','Andhra Pradhesh','Arunachal Pradesh','Assam','Bihar','Chandigarh','Chhattisgarh',
     'Dadra and Nagar Haveli','Damn anad Diu','Delhi','Goa','Gujarat','Haryana','Himachal Predesh','Jammu and Kashmir'
     ,'Jharkhand','Karnataka','Kerala','Ladakh','Lakshadweep','Madhya Predesh','Maharashtra','Manipur',
@@ -272,13 +354,16 @@ def addsuppliers():
     'Uttar Predesh','Uttarakhand','West Bengal','Other Territory']
     estate=ttk.Combobox(hd1,values=stvalues)
     estate.current(0) 
-    estate.place(relx=0.02,rely=0.64,relwidth=0.3,relheight=0.035) 
+    estate.place(relx=0.02,rely=0.63,relwidth=0.3,relheight=0.025) 
 
-    tk.Label(hd1,text='Pin Code',bg='#243e54',font=('times new roman', 14)).place(relx=0.35,rely=0.61)
-    epin=StringVar()
-    tk.Entry(hd1,textvariable=epin).place(relx=0.35,rely=0.64,relwidth=0.3,relheight=0.035)
+    tk.Label(hd1,text='Pin Code',bg='#243e54',font=('times new roman', 14)).place(relx=0.35,rely=0.60)
+    epin=tk.Entry(hd1)
+    epin.place(relx=0.35,rely=0.63,relwidth=0.3,relheight=0.025)
 
-    tk.Label(hd1,text='Country',bg='#243e54',font=('times new roman', 14)).place(relx=0.68,rely=0.61)
+    chk_b3=tk.Label(hd1,text='',font=('arial',11),fg='red',bg='#243e54')
+    chk_b3.place(relx=0.35,rely=0.66)
+
+    tk.Label(hd1,text='Country',bg='#243e54',font=('times new roman', 14)).place(relx=0.68,rely=0.60)
     ctvalues=['choose','Afghanistan','Albania','Algeria','American Samoa','Andorra','Anguilla','Argentina','Aruba','Australia','Austria','Azerbaijan',
     'Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia & Herzegovina','Botswana',
     'Bulgaria','Burundi','Cameroon','Canada','Canary Islands','Cape Verde','Chad','Channel Islands','Cape Verde','Cayman Islands','Channel Islands',
@@ -291,20 +376,25 @@ def addsuppliers():
     'Marshall Island','Martinique','Mauritania','Mauritius','Mayotte']
     econt=ttk.Combobox(hd1,values=ctvalues)
     econt.current(0)
-    econt.place(relx=0.68,rely=0.64,relwidth=0.3,relheight=0.035)
+    econt.place(relx=0.68,rely=0.63,relwidth=0.3,relheight=0.025)
 
-    tk.Label(hd1,text='Notes',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.68)
-    enotes=StringVar()
-    tk.Entry(hd1,textvariable=enotes).place(relx=0.02,rely=0.71,relwidth=0.8,relheight=0.045) 
+    tk.Label(hd1,text='Notes',bg='#243e54',font=('times new roman', 14)).place(relx=0.02,rely=0.67)
+    enotes=tk.Entry(hd1)
+    enotes.place(relx=0.02,rely=0.70,relwidth=0.71,relheight=0.035) 
+    chh=IntVar()
+    ch=Checkbutton(hd1,variable=chh, text = "Agree to Terms and Conditions",bg='#243e54',font=('times new roman', 14),onvalue=1,offvalue=0)
+    ch.place(relx=0.02,rely=0.75)  
+    
+    chk_b4=tk.Label(hd1,text='',font=('arial',12),fg='red',bg='#243e54')
+    chk_b4.place(relx=0.02,rely=0.80)
 
-    Checkbutton(hd1, text = "Agree to Terms and Conditions",bg='#243e54',font=('times new roman', 12)).place(relx=0.02,rely=0.76)  
-
-
-    sub=tk.Button(hd1,text='SUBMIT',font=15,bg='#243e54',command=enter).place(relx=0.5,rely=0.79)
+    sub=tk.Button(hd1,text='SUBMIT',font=15,bg='#243e54',command=enter).place(relx=0.5,rely=0.8)
 
     hd1.place(relx=0.1,rely=0.15,relwidth=0.8,relheight=0.9)
     
-    tk.Frame(frame,bg='#2f516f').place(relx=0,rely=0.92,relwidth=1,relheight=0.08)
+    tk.Frame(frame,bg='#2f516f').place(relx=0,rely=0.92,relwidth=1,relheight=0.09)
+
+
     B.mainloop()
 def sherrymain():
 
