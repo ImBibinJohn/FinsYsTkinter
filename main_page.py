d5dbe6b9d21a25a6e52ec91b58eb0b469f879eb6
Bibin John <91671026+ImBibinJohn@users.noreply.github.com>
2022-05-02 02:34:54 +0530

    initial

--- main_page.py
+++ main_page.py
@@ -1,11 +1,40 @@
+import tkinter as tk
+import webbrowser
 from tkinter import *
+from tkinter import ttk, filedialog, scrolledtext, messagebox
+from datetime import date
+from tkcalendar import DateEntry
+import datetime
+import center_tk_window
+import os
+from tkinter.filedialog import askopenfile
+from tkinter import filedialog as fd
 
-root = Tk()
-root.geometry('500x500')
-root.title("Registration Form")
+root = tk.Tk()
+root.title('FinsYs Tkinter')
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry("%dx%d" % (width, height))
+
+tabControl = ttk.Notebook(root)
+invs= ttk.Frame(tabControl)
+inv=PhotoImage(file='invoice.png')
+invo=inv.subsample(1,1)
+tabControl.add(invs, text ='Invoices',image=invo,compound=LEFT)
+
+invs = ttk.Frame(tabControl)
+tabControl.add(invs, text='Invoices', compound=LEFT)
+
+invs = ttk.Frame(tabControl)
+tabControl.add(invs, text='Invoices', compound=LEFT)
 
 
-label = Label(root, text="Registration form",width=20,font=("bold", 20))
-label.grid(padx=90,pady=53)
+tabControl.pack(expand=1, fill="both", pady=20)
+
+frame0 = tk.Frame(invs)
+frame0.pack(fill=tk.X)
+c1 = Label(frame0)
+
+c1.pack(side=tk.LEFT)
 
 root.mainloop()
