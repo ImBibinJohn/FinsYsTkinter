3bc20b77cced22d82144a578a2feebfd74b3b5a8
Nirmal <nirmalgt.cec@gmail.com>
2022-05-06 19:36:28 +0530

    frontend & backend update

--- chart0faccounts.py
+++ chart0faccounts.py
@@ -14,7 +14,7 @@ mydata = mysql.connector.connect(
     host='localhost', user='root', password='', database='finsys_tkinter')
 cur = mydata.cursor()
 
-# yyyyyy
+# yyyyyyyyyyyygj
 
 
 def selected(event):
