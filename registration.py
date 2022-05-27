66ea4461d2227905fcb290782677603be707639e
sreeja713 <sreejammanikandan713@gmail.com>
2022-05-27 21:08:30 +0530

    registrationform started

---
+++ registration.py
@@ -0,0 +1,13 @@
+from atexit import register
+from tkinter import *
+import tkinter.font as font
+from  tkinter import ttk
+import tkinter as tk
+registeration =tk.Tk()
+
+registeration.title('fynsYs')
+registeration.geometry("2000x2000")
+registeration['bg']='#2f516a'
+headingfont=font.Font(family='Helvitica', size=25,)
+
+registeration.mainloop()
