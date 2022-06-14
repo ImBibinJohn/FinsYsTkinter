5482bdea1d88391ec663cd90dfa24ef1aac5c4e5
Abin KS <abinks056@gmail.com>
2022-06-14 20:13:36 +0530

    basepage changed

--- base2.py
+++ base2.py
@@ -1,137 +1,51 @@
-# # Firstly import Tkinter Module 
-# from tkinter import * 
-# from tkinter.ttk import *
-# import tkinter as tk
-# # Then create a program Tkinter window
-# program = Tk()   
-# # Photoimage class is created
-# # And Image should be in the same folder where there is script saved 
-# p1 = PhotoImage(file = 'logo-icon.png')   
-# # Icon set for program window
-# program.iconphoto(False, p1)   
-# # Button creation
-# b = Button(program, text = 'Press Me!') 
-# b.pack(side = TOP) 
-# program.title('iconphoto() method')
-# mainloop()
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
 
+root = tk.Tk()
+root.geometry("800x400")
 
+# Create Toolbar
+# Toolbar images
+img1 = tk.PhotoImage(file = r"default.png")
+img2 = tk.PhotoImage(file = r"default.png")
+img3 = tk.PhotoImage(file = r"default.png")
 
-# # importing only those functions
-# # which are needed
-# from tkinter import *
-# from tkinter.ttk import *
+toolbar = tk.Frame(root, bd=1, relief=RAISED) 
+toolbar.pack(side=TOP, fill=X)
 
-# # creating tkinter window
-# root = Tk()
+menubtn1 = tk.Menubutton(toolbar, relief=FLAT, compound= LEFT, 
+text="",image=img1)
+menubtn1.pack(side=LEFT, padx=0, pady=0)
 
-# # Adding widgets to the root window
-# Label(root, text = 'GeeksforGeeks', font =(
-# 'Verdana', 15)).pack(side = TOP, pady = 10)
+menubtn2 = tk.Menubutton(toolbar, relief=FLAT, compound= LEFT, 
+text="", image=img2)
+menubtn2.pack(side=LEFT, padx=0, pady=0)
 
-# # Creating a photoimage object to use image
-# photo = PhotoImage(file = r"cogwheel.png")
+menubtn3 = tk.Menubutton(toolbar, relief=FLAT, compound= LEFT, 
+text="", image=img3)
+menubtn3.pack(side=LEFT, padx=0, pady=0)
 
-# # Resizing image to fit on button
-# photoimage = photo.subsample(3, 3)
+# drop down menubtn1
+menu = tk.Menu(menubtn1, tearoff=0)
+menubtn1.config(menu=menu)
 
-# # here, image option is used to
-# # set image on button
-# # compound option is used to align
-# # image on LEFT side of button
-# Button(root, text = 'Click Me !', image = photoimage,
-# 					compound = LEFT).pack(side = TOP)
+menu.insert_command(0, label='Submit', command=lambda: 
+print('Submit clicked'))
+imgvar1 = tk.PhotoImage(file=r'default.png') 
+menu.insert_cascade(1, image=imgvar1, command=lambda: print('Im pop out clicked'))
 
-# mainloop()
+imgvar2 = tk.PhotoImage(file=r'default.png')
+menu.insert_cascade(2, image=imgvar2, command=lambda: print('Im pop out clicked'))
 
+imgvar3 = tk.PhotoImage(file=r'default.png')
+menu.insert_cascade(3, image=imgvar3, command=lambda: print('Im pop out clicked'))
 
+imgvar4 = tk.PhotoImage(file=r'default.png')
+menu.insert_cascade(4, image=imgvar4, command=lambda: print('Im pop out clicked'))
+menu.insert_separator(4)
 
+imgvar5 = tk.PhotoImage(file=r'default.png')
+menu.insert_cascade(5, image=imgvar5, command=lambda: print('Im pop out clicked'))
 
-#Define the working of the button
-#Import all the necessary libraries
-# from tkinter import *
-
-# #Define the tkinter instance
-# win= Toplevel()
-# win.title("Rounded Button")
-
-# #Define the size of the tkinter frame
-# win.geometry("700x300")
-
-# #Define the working of the button
-
-# def my_command():
-#    text.config(text= "You have clicked Me...")
-
-# #Import the image using PhotoImage function
-# click_btn= PhotoImage(file='cogwheel.png')
-
-# #Let us create a label for button event
-# img_label= Label(image=click_btn)
-
-# #Let us create a dummy button and pass the image
-# button= Button(win, image=click_btn,command= my_command,
-# borderwidth=0)
-# button.pack(pady=30)
-
-# text= Label(win, text= "")
-# text.pack(pady=30)
-
-
-# win.mainloop()
-
-
-
-
-# Importing Tkinter module
-# from tkinter import *
-# from tkinter.ttk import *
-
-# # Creating master Tkinter window
-# master = Tk()
-
-# # Creating object of photoimage class
-# # Image should be in the same folder
-# # in which script is saved
-# p1 = PhotoImage(file = 'cogwheel.png')
-
-# # Setting icon of master window
-# master.iconphoto(False, p1)
-
-# # Creating button
-# b = Button(master, text = 'Click me !')
-# b.pack(side = TOP)
-
-# # Infinite loop can be terminated by
-# # keyboard or mouse interrupt
-# # or by any predefined function (destroy())
-# mainloop()
-
-
-
-# import tkinter as tk
-# my_w=tk.Tk()
-# from PIL import Image,ImageTk
-# my_w.geometry('300x100')
-# my_w.title('www.plus2net.com')
-# my_img = ImageTk.PhotoImage(Image.open("logo-icon.png"))
-# b1=tk.Button(my_w,image=my_img)
-# b1.grid(row=1,column=1)
-# my_w.mainloop()
-
-
-# import tkinter  as tk 
-# from tkinter import ttk
-# from PIL import Image,ImageTk
-# my_w = tk.Tk()
-# my_w.geometry("840x570")  
-# #my_img = tk.PhotoImage(file = "D:\\top2.png") 
-# my_img2 = ImageTk.PhotoImage(Image.open("logo-icon.png"))
-
-# bg = tk.Label(my_w, image=my_img2)
-# bg.place(x=0, y=0, relwidth=1, relheight=1)
-
-
-        
-
-# my_w.mainloop()
+root.mainloop()
